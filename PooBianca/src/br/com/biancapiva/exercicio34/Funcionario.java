2903507290dcc5fcddb3e1b8505b6fda9912c6c8
Bianca <130417144+Durato@users.noreply.github.com>
2025-03-27 19:33:38 -0300

    att

--- PooBianca/src/br/com/biancapiva/exercicio34/Funcionario.java
+++ PooBianca/src/br/com/biancapiva/exercicio34/Funcionario.java
@@ -2,13 +2,29 @@ package br.com.biancapiva.exercicio34;
 
 public class Funcionario {
     private String nome;
-    private static int totalFuncionarios;
+    private static int totalFuncionarios = 0;
 
     public Funcionario() {
+    this.totalFuncionarios += 1 ;
     }
 
     public Funcionario(String nome) {
         this.nome = nome;
     }
-    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public static int getTotalFuncionarios() {
+        return totalFuncionarios;
+    }
+
+    public static void setTotalFuncionarios(int totalFuncionarios) {
+        Funcionario.totalFuncionarios = totalFuncionarios;
+    }
 }
---
+++ PooBianca/src/br/com/biancapiva/exercicio34/Principal.java
@@ -0,0 +1,12 @@
+package br.com.biancapiva.exercicio34;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Funcionario funcionario = new Funcionario();
+        Funcionario funcionario2 = new Funcionario();
+        Funcionario funcionario3 = new Funcionario();
+
+        System.out.println(funcionario.getTotalFuncionarios());
+    }
+}
